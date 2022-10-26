e428338f611318bc46b29f4519fc8727a03af8e1
BandersGit <andy.i.flaskan@gmail.com>
2022-10-26 12:59:36 +0200

    Class that siwtches ints

--- app/src/main/java/datatypesexcercise/App.java
+++ app/src/main/java/datatypesexcercise/App.java
@@ -3,7 +3,8 @@ package datatypesexcercise;
 public class App {
     public static void main(String[] args) {
         MinorOrNot check = new MinorOrNot();
-
-        check.CheckIfMinorOrNot();
+        IntSwitch intCheck = new IntSwitch();
+        //check.CheckIfMinorOrNot();
+        intCheck.IntSwitch();
     }
 }
---
+++ app/src/main/java/datatypesexcercise/IntSwitch.java
@@ -0,0 +1,21 @@
+package datatypesexcercise;
+
+public class IntSwitch {
+
+    public void IntSwitch(){
+        int a = 10;
+        int b = 20;
+
+        System.out.print("a = " + a);
+        System.out.print(" ");
+        System.out.println("b = " + b);
+
+        a = b;
+        b -= 10;
+
+        System.out.print("a = " + a);
+        System.out.print(" ");
+        System.out.println("b = " + b);
+
+    }
+}
